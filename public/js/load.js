cd78ad12b9b6c1c86bacfe418a89d638f3eea48c
rainlotus97 <s0704127x@163.com>
2024-01-06 19:30:12 +0800

    第一次提交

---
+++ public/js/load.js
@@ -0,0 +1,94 @@
+// 用于解析Markdown文档
+function fillMarkdownContent(filename) {
+  var xmlhttp;
+
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    highlight: function (code, language) {
+      const validLanguage = hljs.getLanguage(language) ? language : "javascript";
+      return hljs.highlightAuto(validLanguage, code).value;
+    },
+    pedantic: false,
+    gfm: true,
+    breaks: true,
+    sanitize: true,
+    smartLists: true,
+    smartypants: true,
+    xhtml: false,
+  });
+  
+  if (window.XMLHttpRequest) {
+    // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    // IE6, IE5 浏览器执行代码
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  // 得到服务器响应后，对得到的Markdown文档进行解析
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      const markdownContent = xmlhttp.responseText;
+      const pattern = /^---([\s\S]*?)---/;
+      const match = markdownContent.match(pattern);
+      let updatedMarkdownContent = markdownContent;
+      if (match) {
+        const metadataBlock = match[0];
+
+        // 从原始字符串中移除匹配到的部分
+        updatedMarkdownContent = markdownContent.replace(metadataBlock, "");
+        console.log("截取到的 metadataBlock:", metadataBlock);
+      }
+      let htmlStr = marked.marked(updatedMarkdownContent);
+      console.log("updatedMarkdownContent:", updatedMarkdownContent);
+      // 处理img标签
+      htmlStr = solveMdStr(htmlStr);
+      document.getElementById("view").innerHTML = htmlStr;
+      //  设置code高亮
+      hljs.highlightAll();
+      // 触发图片懒加载
+      initObesrver();
+    }
+  };
+  // 向服务器发送请求，获取你需要的Markdown文档
+  xmlhttp.open("GET", "public/md/" + filename + ".md", true);
+  xmlhttp.send();
+}
+
+function solveMdStr(mdStr) {
+  // 使用正则表达式匹配 <img> 标签的内容和 src 属性
+  const regex = /<img[^>]+src="([^"]+)"[^>]*>/g;
+  mdStr = mdStr.replace(regex, (match, src) => {
+    return match.replace(
+      `src="${src}"`,
+      `data-has-lazy-src=false data-src="${src}" src=""`
+    );
+  });
+  return mdStr;
+}
+
+function initObesrver() {
+  let imgs = document.querySelectorAll("img");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // 元素进入视口
+          let hasLaySrc = entry.target.getAttribute("data-has-lazy-src");
+          if (hasLaySrc === "false") {
+            entry.target.src = entry.target.getAttribute("data-src");
+            entry.target.removeAttribute("data-src");
+            entry.target.setAttribute("data-has-lazy-src", true);
+            console.log(entry.target);
+          }
+        }
+      });
+    },
+    { threshold: 1 }
+  );
+  if (imgs.length > 0) {
+    imgs.forEach((img) => {
+      observer.observe(img);
+    });
+  }
+}
---
+++ public/md/OnlineMarkdown.md
@@ -0,0 +1,48 @@
+# markdown渲染
+
+## 一、涉及功能
+
+- 图片懒加载——原生API方法监听可视窗口内容的加载
+- 读取本地md文档（读取在线md需要自行解决跨域问题）——涉及marked解析
+- code代码高亮——highlight处理样式
+
+## 二、样式展示
+
+### 2.1 表格
+
+| 名称 | 必选 | 描述 |
+| ---- | ---- | ---- |
+| name | M    | 名称 |
+| age  | O    | 年龄 |
+| sex  | M    | 性别 |
+
+### 2.2 代码块
+
+```javascript
+function initObesrver() {
+  let imgs = document.querySelectorAll("img");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // 元素进入视口
+          let hasLaySrc = entry.target.getAttribute("data-has-lazy-src");
+          if (hasLaySrc === "false") {
+            entry.target.src = entry.target.getAttribute("data-src");
+            entry.target.removeAttribute("data-src");
+            entry.target.setAttribute("data-has-lazy-src", true);
+            console.log(entry.target);
+          }
+        }
+      });
+    },
+    { threshold: 1 }
+  );
+  if (imgs.length > 0) {
+    imgs.forEach((img) => {
+      observer.observe(img);
+    });
+  }
+}
+```
+
