f2d00e0b92aa42201be2c1f413dfb5c4cb78dc58
rainlotus97 <s0704127x@163.com>
2024-01-06 20:49:33 +0800

    支持本地和在线两种方式获取md

--- public/js/load.js
+++ public/js/load.js
@@ -1,10 +1,10 @@
 /**
  * 初始化服务
  * @param {string} filenameOrUrl
- * @param {boolean} isOnline 
+ * @param {boolean} isOnline
  * @description isOnline 为true时，filenameOrUrl为url，为false时，filenameOrUrl为文件名
  */
-function initService(filenameOrUrl, isOnline=false) {
+function initService(filenameOrUrl, isOnline = false) {
   // 设置marked
   marked.setOptions({
     renderer: new marked.Renderer(),
@@ -22,47 +22,72 @@ function initService(filenameOrUrl, isOnline=false) {
     smartypants: true,
     xhtml: false,
   });
-  this.fillMarkdownContentByOnlineFile(filenameOrUrl, isOnline);
+  if (isOnline) {
+    fillMarkdownContentByOnlineFile(filenameOrUrl);
+  } else {
+    fillMarkdownContent(filenameOrUrl);
+  }
 }
 
 /**
  * 通过在线文件填充markdown内容
  * @param {string} filenameOrUrl
  * @param {boolean} isOnline
- * @description 针对md作者信息单独处理，md作者信息需要放在md文件的头部，以---开头，以---结尾
  */
-function fillMarkdownContentByOnlineFile(filenameOrUrl, isOnline) {
+function fillMarkdownContentByOnlineFile(readmeUrl) {
   // 本地文件需要拼接路径
-  let readmeUrl = isOnline
-    ? filenameOrUrl
-    : "/public/md/" + filenameOrUrl + ".md";
-  console.log("readmeUrl:", readmeUrl);
   fetchOnlineMdFile(readmeUrl)
     .then((data) => {
-      const markdownContent = data;
-      const pattern = /^---([\s\S]*?)---/;
-      const match = markdownContent.match(pattern);
-      let updatedMarkdownContent = markdownContent;
-      if (match) {
-        const metadataBlock = match[0];
-        // 从原始字符串中移除匹配到的部分
-        updatedMarkdownContent = markdownContent.replace(metadataBlock, "");
-        console.log("截取到的 metadataBlock:", metadataBlock);
-      }
-      let htmlStr = marked.marked(updatedMarkdownContent);
-      // 处理img标签
-      htmlStr = solveMdStr(htmlStr);
-      document.getElementById("view").innerHTML = htmlStr;
-      //  设置code高亮
-      hljs.highlightAll();
-      // 触发图片懒加载
-      initObesrver();
+      initMarkedService(data);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
+/**
+ * 获取本地md文件并填充markdown内容
+ * @param {string} filename
+ */
+function fillMarkdownContent(filename) {
+  let xmlhttp = window.XMLHttpRequest
+    ? new XMLHttpRequest()
+    : new ActiveXObject("Microsoft.XMLHTTP");
+  // 得到服务器响应后，对得到的Markdown文档进行解析
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      const markdownContent = xmlhttp.responseText;
+      initMarkedService(markdownContent);
+    }
+  };
+  // 向服务器发送请求，获取你需要的Markdown文档
+  xmlhttp.open("GET", "public/md/" + filename + ".md", true);
+  xmlhttp.send();
+}
+
+/**
+ * @param {string} markdownContent
+ * @description 对md统一处理，包括处理md作者信息，处理img标签，设置code高亮，触发图片懒加载
+ */
+function initMarkedService(markdownContent) {
+  const pattern = /^---([\s\S]*?)---/;
+  const match = markdownContent.match(pattern);
+  let updatedMarkdownContent = markdownContent;
+  if (match) {
+    const metadataBlock = match[0];
+    // 从原始字符串中移除匹配到的部分
+    updatedMarkdownContent = markdownContent.replace(metadataBlock, "");
+    console.log("截取到的 metadataBlock:", metadataBlock);
+  }
+  let htmlStr = marked.marked(updatedMarkdownContent);
+  // 处理img标签
+  htmlStr = solveMdStr(htmlStr);
+  document.getElementById("view").innerHTML = htmlStr;
+  //  设置code高亮
+  hljs.highlightAll();
+  // 触发图片懒加载
+  initObesrver();
+}
 /**
  * 处理md字符串
  * @param {string} mdStr
@@ -109,6 +134,11 @@ function initObesrver() {
   }
 }
 
+/**
+ * 获取在线md文件
+ * @param {string} readmeUrl
+ * @description readmeUrl为在线md文件的url,github返回的为json，其他为text
+ */
 function fetchOnlineMdFile(readmeUrl) {
   return new Promise((resolve, reject) => {
     fetch(readmeUrl)
@@ -116,11 +146,35 @@ function fetchOnlineMdFile(readmeUrl) {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        return response.text();
+        // 对GitHub的地址进行处理
+        let isGithub = readmeUrl.includes("github.com") ? true : false;
+        return isGithub ? response.json() : response.text();
       })
       .then((data) => {
-        resolve(data);
+        if (isGithub) {
+          // 使用 TextDecoder 解码 Base64 编码的内容，指定字符集为 UTF-8
+          const decodedContent = new TextDecoder("utf-8").decode(
+            base64ToArrayBuffer(data.content)
+          );
+          console.log(decodedContent);
+          resolve(decodedContent);
+        } else {
+          resolve(data);
+        }
       })
       .catch((error) => reject(error));
   });
+}
+
+// 将 Base64 编码的字符串转换为 ArrayBuffer
+function base64ToArrayBuffer(base64) {
+  const binaryString = atob(base64);
+  const length = binaryString.length;
+  const bytes = new Uint8Array(length);
+
+  for (let i = 0; i < length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+
+  return bytes.buffer;
 }
